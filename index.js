37dd152dd000afa015385a4716abcdd9fb8b2610
Muhitori <turevichkolya@gmail.com>
2023-06-05 16:58:37 +0300

    syndore notificator complete

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const express = require("express");
+import { Client, Events, Partials, GatewayIntentBits } from "discord.js";
+import SteamUser from "steam-user";
 
-const app = express();
-const port = process.env.PORT || 3000;
+const { TARGET_ID, MESSAGE, DISCORD_TOKEN, STEAM_USERNAME, STEAM_PASSWORD } =
+	process.env;
 
-app.get("/", (req, res) => {
-	res.send("Ok");
+const steamClient = new SteamUser();
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessageTyping,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.DirectMessageReactions,
+		GatewayIntentBits.DirectMessageTyping,
+	],
+	partials: [Partials.Channel, Partials.Message, Partials.Reaction],
 });
 
-app.use(express.static("public"));
-app.use(express.json());
+steamClient.logOn({
+	accountName: STEAM_USERNAME,
+	password: STEAM_PASSWORD,
+});
 
-app.listen(port, () => console.log(`Listening on ${port}`));
+steamClient.on("friendMessageEcho", async (steamID, message) => {
+	console.log(`Sent message to ${steamID}: ${message}`);
+
+	const user = await client.users.fetch(TARGET_ID);
+	await user.send(MESSAGE).catch((err) => {});
+});
+
+client.once(Events.ClientReady, (c) => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(DISCORD_TOKEN);
